f83728feab028ef5b87c71d718028a525d74af75
fenqiu <fenqiu@pinduoduo.com>
2020-02-25 10:27:11 +0800

    add pupeterSpider

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,9 @@
 import Router from "koa-router";
-import { spider } from "./controller";
+import { spider, puppeteerSpider } from "./controller";
 
 const router = new Router({ prefix: "" });
 
 router.get("/spider/init", ...([spider.init] as any));
+router.get("/puppeteerSpider/init", ...([puppeteerSpider.init] as any));
 
 export { router };
